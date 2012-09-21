6fac94d2653701f1e42cad6ed767e72e27de8d1d
grdk <grdk.prime@gmail.com>
2012-09-21 07:53:10 -0700

    Enable my personal storylines

--- Questor/Storylines/Storyline.cs
+++ Questor/Storylines/Storyline.cs
@@ -49,7 +49,7 @@ namespace Questor.Storylines
                                //{"A Special Delivery", new GenericCourier()}, // Needs 40k m3 cargo capacity (i.e. Iteron Mark V, T2 CHO rigs)
                                /* COURIER/DELIVERY - AMARR */
                                {"Opiate of the Masses", new GenericCourier()},
-                               {"Send the Marines", new GenericCourier()},
+                               {"Send the Marines!", new GenericCourier()},
                                {"The Governors Ball", new GenericCourier()},
                                {"The State of the Empire", new GenericCourier()},
                                {"Unmasking the Traitor", new GenericCourier()},
@@ -79,21 +79,21 @@ namespace Questor.Storylines
                                {"Patient Zero", new GenericCombatStoryline()},
                                {"Record Cleaning", new GenericCombatStoryline()},
                                {"Shipyard Theft", new GenericCombatStoryline()},
-                               ////{"Soothe the Salvage Beast", new GenericCombatStoryline()},
+                               {"Soothe the Salvage Beast", new GenericCombatStoryline()},
                                /* COMBAT - AMARR */
                                {"Blood Farm", new GenericCombatStoryline()},
                                {"Dissidents", new GenericCombatStoryline()},
-                               ////{"Extract the Renegade", new GenericCombatStoryline()},
-                               ////{"Gate to Nowhere", new GenericCombatStoryline()},
+                               {"Extract the Renegade", new GenericCombatStoryline()},
+                               {"Gate to Nowhere", new GenericCombatStoryline()},
                                ////{"Racetrack Ruckus", new GenericCombatStoryline()},
                                {"The Mouthy Merc", new GenericCombatStoryline()},
                                /* COMBAT - CALDARI */
                                {"Crowd Control", new GenericCombatStoryline()},
-                               ////{"Forgotten Outpost", new GenericCombatStoryline()},
-                               ////{"Illegal Mining", new GenericCombatStoryline()},
-                               ////{"Innocents in the Crossfire", new GenericCombatStoryline()},
+                               {"Forgotten Outpost", new GenericCombatStoryline()},
+                               //{"Illegal Mining", new GenericCombatStoryline()}, // Extremely high DPS after shooting structures!
+                               {"Innocents in the Crossfire", new GenericCombatStoryline()},
                                ////{"Jealous Rivals", new GenericCombatStoryline()},
-                               ////{"Stem the Flow", new GenericCombatStoryline()},
+                               {"Stem the Flow", new GenericCombatStoryline()},
                                /* COMBAT - GALLENTE */
                                ////{"Kidnappers Strike - Ambush In The Dark (1 of 10)", new GenericCombatStoryline()},
                                ////{"Kidnappers Strike - The Kidnapping (3 of 10)", new GenericCombatStoryline()},
@@ -106,14 +106,14 @@ namespace Questor.Storylines
                                ////{"Whispers in the Dark - Lay and Pray (2 of 4)", new GenericCombatStoryline()},
                                ////{"Whispers in the Dark - The Outpost (4 of 4)", new GenericCombatStoryline()},
                                /* COMBAT - MINMATAR */
-                               ////{"Amarrian Excavators", new GenericCombatStoryline()},
+                               {"Amarrian Excavators", new GenericCombatStoryline()},
                                ////{"Diplomatic Incident", new GenericCombatStoryline()},
-                               {"Matriarch", new GenericCombatStoryline()},
+                               {"Matriarch", new GenericCombatStoryline()},  
                                {"Nine Tenths of the Wormhole", new GenericCombatStoryline()},
                                {"Postmodern Primitives", new GenericCombatStoryline()},
                                {"Quota Season", new GenericCombatStoryline()},
                                {"The Blood of Angry Men", new GenericCombatStoryline()},
-                               /* COMBAT - MORE THAN ONE RACE */                                                            
+                               /* COMBAT - MORE THAN ONE RACE */                                                         
                             };
         }
 
